3b65b76644f1ec2ec0757699e41f58f814c092f9
Trevis Thomas <trevislynnthomas@gmail.com>
2018-09-15 12:56:40 -0600

    Working Template

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import { Link } from "react-router-dom";
+// import { Col, Row, Container } from "../../components/Grid";
+
+class Home extends Component {
+    state = {
+    };
+
+    render() {
+        return (
+            <h1>Hello World</h1>
+        );
+    }
+}
+
+export default Home;
