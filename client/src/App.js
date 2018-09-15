7545e9abeb2dd8bd20275b6bed421baafc03d912
Trevis Thomas <trevislynnthomas@gmail.com>
2018-09-15 13:03:36 -0600

    Nav Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import Nav from "./components/Nav";
 
 const App = () => (
   <Router>
     <div>
-      {/* <Nav /> */}
+      <Nav />
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
