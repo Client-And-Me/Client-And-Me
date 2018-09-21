81b55aa18631e33668749c8aad95d15aadd3954a
aeffiong <aeffiong9@gmail.com>
2018-09-20 21:09:38 -0600

    client page components

--- client/src/components/ProfileText/ProfileText.js
+++ client/src/components/ProfileText/ProfileText.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./ProfileText.css";
 
-const ProfileText = () => (
+const ProfileText = props => (
   
     <div className="col s6 m2">
       <div className="card blue-grey darken-1">
         <div className="card-content white-text">
           <span className="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+          <p>{props.children}</p>
         </div>
       </div>
     </div>
