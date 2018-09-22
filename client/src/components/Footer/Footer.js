39a546a9afcaa4cd0ec4df80499f2eec682a2454
Trevis Thomas <trevislynnthomas@gmail.com>
2018-09-22 10:44:06 -0600

    Intial react-materialize merge

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,32 +1,23 @@
 import React from "react";
 import "./Footer.css";
+import { Footer } from "react-materialize";
 
-const Footer = () => (
-    <footer className="page-footer">
-    <div className="container">
-      <div className="row">
-        <div className="col l6 s12">
-          <h5 className="white-text">Footer Content</h5>
-          <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-        </div>
-        <div className="col l4 offset-l2 s12">
-          <h5 className="white-text">Links</h5>
-          <ul>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-            <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div className="footer-copyright">
-      <div className="container">
-      © 2018 Copyright Text
+export default () => (
+  <Footer copyrights="&copy 2015 Copyright Text"
+    moreLinks={
       <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
-      </div>
-    </div>
-  </footer>
-);
-
-export default Footer;
+    }
+    links={
+      <ul>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+        <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
+      </ul>
+    }
+    className='example'
+  >
+    <h5 className="white-text">Footer Content</h5>
+    <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+  </Footer>
+)
