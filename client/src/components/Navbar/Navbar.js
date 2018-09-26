02c55aaffcda28f43abedabe0c6dc73402e638d9
Trevis Thomas <trevislynnthomas@gmail.com>
2018-09-25 21:22:18 -0600

    Home Page Design Work

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import "./Navbar.css";
+import "./Navbar.css";
 import { Navbar, NavItem } from "react-materialize";
 
 
 export default () => (
-  <Navbar brand='client&me' right>
-    <NavItem href='/SignIn'>Login</NavItem>
-    <NavItem href='/register'>Register</NavItem>
-  </Navbar>
+  <nav>
+    <div class="nav-wrapper navbar">
+      <a href="/" class="brand-logo">client&me</a>
+      <ul id="nav-mobile" class="right hide-on-large-and-up">
+        <li><a href="/SignIn">Login</a></li>
+        <li><a href="/register">Register</a></li>
+      </ul>
+    </div>
+  </nav>
 )
