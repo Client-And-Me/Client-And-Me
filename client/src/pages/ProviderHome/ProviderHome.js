64eea9a0a8799488ace20732d044b9bf62d018d6
Trevis Thomas <trevislynnthomas@gmail.com>
2018-09-29 09:59:26 -0600

    Made Some Things Pretty

--- client/src/pages/ProviderHome/ProviderHome.js
+++ client/src/pages/ProviderHome/ProviderHome.js
@@ -6,6 +6,7 @@ import Modal from "../../components/Modal"
 import "./ProviderHome.css"
 import { CardTitle, Card, Col, Row, Section } from "react-materialize";
 import NavbarProvider from "../../components/Navbar/NavbarProvider";
+import AppointmentsTable from "../../components/AppointmentsTable"
 
 
 class ProviderHome extends Component {
@@ -16,23 +17,30 @@ class ProviderHome extends Component {
         return (
             <div>
                 <NavbarProvider />
-            <Section>
-                <Row>
-                    <Col s={12} m={2} l={2}>
-                        <Card header={<CardTitle reveal image={"./assets/images/female_profile.jpg"} waves='light' />}
-                            title="Name Goes Here"
-                            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-                            <p><a href="#">This is a link</a></p>
-                        </Card>
-                    </Col>
-                    <Col s={12} m={8} l={8}>
-                        <div className="card-panel">
-                            <FullCalendar />
-                        </div>
-                    </Col>
-                </Row>
+                <Section>
+                    <Row>
+                        <Col s={12} m={3} l={3} offset="l1 m1 s0">
+                                <Card header={<CardTitle reveal image={"./assets/images/female_profile.jpg"} waves='light' />}
+                                    title="Name Goes Here"
+                                    reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+                                    <p><a href="#">This is a link</a></p>
+                                </Card>
+                        </Col>
 
-            </Section>
+                        <Col s={12} m={7} l={7} >
+                                <AppointmentsTable />
+                        </Col>
+
+                    </Row>
+                    <Row>
+                        <Col s={12} m={10} l={10} offset="l1 m1 s0">
+                            <div className="card-panel">
+                                <FullCalendar />
+                            </div>
+                        </Col>
+                    </Row>
+
+                </Section>
             </div>
         );
     }
