a5a94529f14f2295c571c6fc6437ce1362374e2f
lizardskins <riley.barlow@gmail.com>
2018-10-02 17:47:45 -0600

    modal pickers working

--- client/src/pages/ProviderHome/ProviderHome.js
+++ client/src/pages/ProviderHome/ProviderHome.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import FullCalendar from "../../components/FullCalendar";
 // import { Link } from "react-router-dom";
 import "./ProviderHome.css"
-import { CardTitle, Card, Col, Row, Section } from "react-materialize";
+import { CardTitle, Card, Col, Row, Section, Input } from "react-materialize";
 import NavbarProvider from "../../components/Navbar/NavbarProvider";
 import AppointmentsTable from "../../components/AppointmentsTable"
 
@@ -26,20 +26,9 @@ class ProviderHome extends Component {
                                 <p><a href="#">This is a link</a></p>
                             </Card>
                         </Col>
-
-                        <Col s={12} m={7} l={7} >
-                            <AppointmentsTable />
-                                <Card className="height" header={<CardTitle reveal image={"./assets/images/female_profile.jpg"} waves='light' />}
-                                    title="Name Goes Here"
-                                    reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
-                                    <p><a href="#">This is a link</a></p>
-                                </Card>
-                        </Col>
-
                         <Col s={12} m={7} l={7}>
-                                <AppointmentsTable />
+                            <AppointmentsTable />
                         </Col>
-
                     </Row>
                     <Row>
                         <Col s={12} m={10} l={10} offset="l1 m1 s0">
@@ -48,7 +37,13 @@ class ProviderHome extends Component {
                             </div>
                         </Col>
                     </Row>
-
+                    <Row>
+                        <Col s={12} m={10} l={10}>
+                            <Row>
+                                <Input name='on' type='date' onChange={function (e, value) { }} />
+                            </Row>
+                        </Col>
+                    </Row>
                 </Section>
             </div>
         );
