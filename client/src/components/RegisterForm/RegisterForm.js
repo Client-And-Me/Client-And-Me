615381e8a1f48d03eac99d51ad1415781338e41c
Trevis Thomas <trevislynnthomas@gmail.com>
2018-10-02 17:48:44 -0600

    Login & Register Forms

--- client/src/components/RegisterForm/RegisterForm.js
+++ client/src/components/RegisterForm/RegisterForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import "./RegisterForm.css";
 import { auth } from '../../firebase';
 
 const INITIAL_STATE = {
@@ -63,44 +63,55 @@ class RegisterForm extends Component {
             username === '';
 
         return (
-            <div class="container">
-                <form onSubmit={this.onSubmit}>
-                    <select>
-                        <option value="" disabled selected>Sign up as Client or Provider</option>
-                        <option value="client">Client</option>
-                        <option value="provider">Provider</option>
-                    </select>
+            <div>
+                <div id="login-page" className="row">
+                    <div className="col s12 m4 l4 offset-l4 offset-m4 z-depth-2 hoverable card-panel loginCard">
+                        <p className="center login-form-text">client&me</p>
+                        <form onSubmit={this.onSubmit}>
+                            <select>
+                                <option value="" disabled selected>Sign up as Client or Provider</option>
+                                <option value="client">Client</option>
+                                <option value="provider">Provider</option>
+                            </select>
 
-                    <input
-                        value={username}
-                        onChange={event => this.setState(byPropKey('username', event.target.value))}
-                        type="text"
-                        placeholder="Full Name" />
+                            <input
+                                value={username}
+                                onChange={event => this.setState(byPropKey('username', event.target.value))}
+                                type="text"
+                                placeholder="Full Name" />
 
-                    <input
-                        value={email}
-                        onChange={event => this.setState(byPropKey('email', event.target.value))}
-                        type="text"
-                        placeholder="Email Address" />
+                            <input
+                                value={email}
+                                onChange={event => this.setState(byPropKey('email', event.target.value))}
+                                type="text"
+                                placeholder="Email Address" />
 
-                    <input
-                        value={passwordOne}
-                        onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
-                        type="password"
-                        placeholder="Password" />
+                            <input
+                                value={passwordOne}
+                                onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
+                                type="password"
+                                placeholder="Password" />
 
-                    <input
-                        value={passwordTwo}
-                        onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
-                        type="password"
-                        placeholder="Confirm Password" />
+                            <input
+                                value={passwordTwo}
+                                onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
+                                type="password"
+                                placeholder="Confirm Password" />
 
-                    <button disabled={isInvalid} type="submit">
-                        Sign Up
-                </button>
+                            {/* <button disabled={isInvalid} type="submit">
+                                Register
+                            </button> */}
 
-                    {error && <p>{error.message}</p>}
-                </form>
+                            <div className="row">
+                                <div className="input-field col s12">
+                                    <button className="btn waves-effect waves-light col s12" disabled={isInvalid} type="submit">Register</button>
+                                </div>
+                            </div>
+
+                            {error && <p>{error.message}</p>}
+                        </form>
+                    </div>
+                </div>
             </div >
         );
     }
